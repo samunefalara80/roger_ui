33ec43c0e3b06b43c9edeaa55e78906389a27866
ingsissam <33227372+ingsissam@users.noreply.github.com>
2018-03-21 09:37:06 -0400

    feat: with emun

--- lib/web/helpers/filter.ex
+++ lib/web/helpers/filter.ex
@@ -1,23 +1,30 @@
 defmodule RogerUI.Web.Helpers.Filter do
   @moduledoc """
-  Takes an enumeration and returns only those elements defined by a field and a filter. 
+  Takes an enumeration and returns only those elements defined by a field and a filter.
   """
   @spec call(
           xs :: Enumerable.t(),
-          field :: String.t(),
+          fields :: List.t(),
           filter :: String.t()
         ) :: []
 
   @doc """
-  Takes an enumerable, a field and a filter and returns it filtered
+  Takes an enumerable, list of  fields and a filter and returns it filtered
   """
   def call(enumerable, _, ""), do: enumerable
 
-  def call(enumerable, field, filter) do
+  def call(enumerable, fields, filter) do
     filter = String.upcase(filter)
 
-    Stream.filter(enumerable, fn e ->
-      e
+    Enum.filter(enumerable, &check(&1, fields, filter))
+  end
+
+  defp check(map, field, filter), do: check(map, [field], filter)
+
+  defp check(map, fields, filter) do
+    fields
+    |> Enum.any?(fn field ->
+      map
       |> Map.get(field)
       |> to_string()
       |> String.upcase()
